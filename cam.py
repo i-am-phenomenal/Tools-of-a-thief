9feaf867c8621048e2758aebd0e6775c1f7954c1
Aditya Chaturvedi <chat29aditya@gmail.com>
2020-08-15 22:13:05 +0530

    WIP

---
+++ cam.py
@@ -0,0 +1,22 @@
+
+
+import cv2
+import Image
+cv2.namedWindow("preview")
+vc = cv2.VideoCapture(0)
+
+if vc.isOpened(): # try to get the first frame
+    rval, frame = vc.read()
+    frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
+    img = Image.fromarray(frame)
+    # return img
+else:
+    rval = False
+
+while rval:
+    cv2.imshow("preview", frame)
+    rval, frame = vc.read()
+    key = cv2.waitKey(20)
+    if key == 27: # exit on ESC
+        break
+cv2.destroyWindow("preview")
