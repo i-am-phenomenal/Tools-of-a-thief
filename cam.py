0700a5e37c88ef0f2f64ca01e63c45dc75edac9a
Aditya Chaturvedi <chat29aditya@gmail.com>
2020-08-16 04:08:18 +0530

    Scheduler for taking screenshot every 2 seconds

--- cam.py
+++ cam.py
@@ -1,6 +1,8 @@
 import cv2
 from pil import  Image
 import os
+import time
+import schedule
 
 def getImagesCount(): 
     DIR = os.getcwd() + "/images"    
@@ -12,22 +14,31 @@ def saveImage(frame):
     if not cv2.imwrite(os.getcwd() + "/images/" + imageName, frame):
         raise Exception("Could not write image")
 
-def launch_webcam():     
-    cv2.namedWindow("preview")
+def launchWebcamAndTakeScreenshot():     
     vc = cv2.VideoCapture(0)
+    vc.set(411, 1.0) # LED Selector
+    vc.set(412, -2.0) # LED Mode
+    vc.set(32, 0.0)
     if vc.isOpened(): # try to get the first frame
         rval, frame = vc.read()
-        frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
-        img = Image.fromarray(frame)        
+        saveImage(frame)
+        print("TOOK SCREENSHOT ")
     else:
         rval = False
-    while rval:
-        cv2.imshow("preview", frame)
-        rval, frame = vc.read()
-        key = cv2.waitKey(20)
-        if key == 27: # exit on ESC
-            saveImage(frame)
-            break
-    cv2.destroyWindow("preview")
-    
-launch_webcam()
+
+    """ Removed code for opening a window """
+    # while rval:
+    #     rval, frame = vc.read()
+    #     key = cv2.waitKey(20)
+    #     schedule.every(2).seconds.do(saveImageSchedule)
+        # if key == 27: # exit on ESC
+        #     saveImage(frame)
+        #     break
+
+""" 
+    Current times is for 2 seconds. Hence, the program will wake up after every 2 seconds and take a screenshot.
+"""
+schedule.every(2).seconds.do(launchWebcamAndTakeScreenshot)
+while True: 
+    schedule.run_pending()
+    time.sleep(1)
